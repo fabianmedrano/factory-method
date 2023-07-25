09429bcc5f8b5695bf1f76b356f9bc96d16bdb35
fabianmedrano <135065707+fabianmedrano@users.noreply.github.com>
2023-07-25 07:40:22 -0600

    Agregar archivos de proyecto.

---
+++ Infantry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method
+{
+    internal class Infantry : MilitaryUnit
+    {
+
+        public override void attack() { 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilitaryUnitFactory infantryFactory = new InfantryFactory();
+
+            MilitaryUnit infantry = infantryFactory.CreateUnit();
+
+
+
+            infantry.attack();
+        }
+    }
+
+    /*
+     
+     public abstract class UnidadMilitar
+{
+    public abstract void Atacar();
+}
+
+public class Infanteria : UnidadMilitar
+{
+    public override void Atacar()
+    {
+        // Lógica para atacar como infantería
+    }
+}
+
+public class Caballeria : UnidadMilitar
+{
+    public override void Atacar()
+    {
+        // Lógica para atacar como caballería
+    }
+}
+
+public class Arquero : UnidadMilitar
+{
+    public override void Atacar()
+    {
+        // Lógica para atacar como arquero
+    }
+}
+
+public abstract class UnidadMilitarFactory
+{
+    public abstract UnidadMilitar CrearUnidad();
+}
+
+public class InfanteriaFactory : UnidadMilitarFactory
+{
+    public override UnidadMilitar CrearUnidad()
+    {
+        return new Infanteria();
+    }
+}
+
+public class CaballeriaFactory : UnidadMilitarFactory
+{
+    public override UnidadMilitar CrearUnidad()
+    {
+        return new Caballeria();
+    }
+}
+
+public class ArqueroFactory : UnidadMilitarFactory
+{
+    public override UnidadMilitar CrearUnidad()
+    {
+        return new Arquero();
+    }
+}
+     
+     
+     */
+}
